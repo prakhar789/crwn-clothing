a0cc6fc30c88b0cf7050696e1da7b776c4596c67
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2020-12-11 11:12:24 +0530

    finishing sign in handleSubmit method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,18 +3,47 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyBrM0dDqdd0eOMSvqk_ehguTVBZdhZIHpI",
-  authDomain: "projects-62658.firebaseapp.com",
-  projectId: "projects-62658",
-  storageBucket: "projects-62658.appspot.com",
-  messagingSenderId: "155595392456",
-  appId: "1:155595392456:web:affee03584bad107a72cfd",
+  apiKey: "AIzaSyDKxkLEgv79tMINp0mE9Xfv77_Uev9XSNE",
+  authDomain: "clwn-clothing-6890b.firebaseapp.com",
+  projectId: "clwn-clothing-6890b",
+  storageBucket: "clwn-clothing-6890b.appspot.com",
+  messagingSenderId: "345214303241",
+  appId: "1:345214303241:web:dab183d5acbe4b492f8cd8",
 };
 
-firebase.initializeApp(config);
-
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+} else {
+  firebase.app(); // if already initialized, use that one
+}
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
 
 // which providre do we wanna use linkedin,fb,google......
 const provider = new firebase.auth.GoogleAuthProvider();
